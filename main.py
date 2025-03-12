e8434aab1fa4d6c856dd67d56a06da9eb716bfa9
Irina M. S <irinamardhatillah@gmail.com>
2025-03-12 11:02:26 +0700

    Add image annotation visualization

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import cv2
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
-
-tree = ET.parse(
-    ".\\dataset\\United_States\\train\\annotations\\xmls\\United_States_000000.xml"
-)
-root = tree.getroot()
 
 
 @dataclass
@@ -32,7 +28,10 @@ class Annotation:
     objects: list[AnnotationObject]
 
 
-def parse(root: ET.Element) -> Annotation:
+def parse(annotation_path: str) -> Annotation:
+    tree = ET.parse(annotation_path)
+    root = tree.getroot()
+
     filename = root.find("filename").text
 
     size = root.find("size")
@@ -65,5 +64,39 @@ def parse(root: ET.Element) -> Annotation:
     return annotation
 
 
-annotation = parse(root)
-print(annotation)
+def visualize_annotation(image_path: str, annotation: Annotation):
+    image = cv2.imread(image_path)
+    for o in annotation.objects:
+        bndbox = o.bndbox
+        cv2.rectangle(
+            img=image,
+            pt1=(bndbox.xmin, bndbox.ymin),
+            pt2=(bndbox.xmax, bndbox.ymax),
+            color=(0, 255, 0),
+            thickness=2,
+        )
+        cv2.putText(
+            img=image,
+            text=o.name,
+            org=(bndbox.xmin, bndbox.ymin - 5),
+            color=(0, 255, 0),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.5,
+            thickness=2,
+        )
+
+    cv2.imshow(annotation.filename, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    image_path = "./dataset/United_States/train/images/United_States_000001.jpg"
+    annotation_path = (
+        "./dataset/United_States/train/annotations/xmls/United_States_000001.xml"
+    )
+
+    annotation = parse(annotation_path)
+    print(annotation)
+
+    visualize_annotation(image_path, annotation)

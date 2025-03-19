39914065ea70e25614a9d7a139b2b6c4134a1692
Irina M. S <irinamardhatillah@gmail.com>
2025-03-19 15:36:03 +0700

    Implement train and test methods

--- main.py
+++ main.py
@@ -1,92 +1,93 @@
+import os
+import argparse
 import torch
 import torch.optim as optim
 
 from torch.utils.data import random_split
-from torch.utils.data import DataLoader
 
 from dataset import RoadDamageDataset
 from model import Model
+from train import train
+from test import test
+from utils import visualize_annotation
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Load the train and validation dataset
-# czech_data = RoadDamageDataset(dir="./dataset/czech", split="train")
-# india_data = RoadDamageDataset(dir="./dataset/india", split="train")
-# japan_data = RoadDamageDataset(dir="./dataset/japan", split="train")
-# norway_data = RoadDamageDataset(dir="./dataset/norway", split="train")
-# us_data = RoadDamageDataset(dir="./dataset/united_states", split="train")
-
-data = RoadDamageDataset(dir="./dataset/united_states", split="train")
-train_size = int(0.8 * len(data))
-val_size = len(data) - train_size
-train_data, val_data = random_split(data, [train_size, val_size])
-
-test_data = RoadDamageDataset(dir="./dataset/united_states", split="test")
-
-train_loader = DataLoader(
-    train_data,
-    batch_size=1,
-    shuffle=True,
-    collate_fn=lambda batch: tuple(zip(*batch)),
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--mode",
+    type=str,
+    choices=["train", "test"],
+    required=True,
+    help="Mode: 'train' or 'test'",
+)
+parser.add_argument(
+    "--device",
+    type=str,
+    default="cuda" if torch.cuda.is_available() else "cpu",
+    help="Device: 'cpu' or 'cuda'",
 )
 
-val_loader = DataLoader(
-    val_data,
-    batch_size=1,
-    shuffle=False,
-    collate_fn=lambda batch: tuple(zip(*batch)),
-)
+args = parser.parse_args()
 
-# Define the model
-model = Model(num_classes=5)  # 4 road damage classes and 1 background
-model.to(device)
+device = torch.device(args.device)
 
-# Optimizer
+model = Model(num_classes=5).to(device)
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
 lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.9)
 
-# Training loop
-num_epochs = 100
+if args.mode == "train":
+    czech_train_data = RoadDamageDataset(dir="./dataset/czech", split="train")
+    india_train_data = RoadDamageDataset(dir="./dataset/india", split="train")
+    japan_train_data = RoadDamageDataset(dir="./dataset/japan", split="train")
+    norway_train_data = RoadDamageDataset(dir="./dataset/norway", split="train")
+    us_train_data = RoadDamageDataset(dir="./dataset/united_states", split="train")
 
-for epoch in range(num_epochs):
-    model.train()
-    total_train_loss = 0
+    train_data = torch.utils.data.ConcatDataset(
+        [
+            czech_train_data,
+            india_train_data,
+            japan_train_data,
+            norway_train_data,
+            us_train_data,
+        ]
+    )
+    train_size = int(0.8 * len(train_data))
+    val_size = len(train_data) - train_size
+    train_data, val_data = random_split(train_data, [train_size, val_size])
 
-    for images, targets in train_loader:
-        images = [i.to(device) for i in images]
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+    print("Dataset parsed successfully. Starting training the model.")
 
-        # Forward pass
-        losses, detections = model(images, targets)
-        loss = sum(l for l in losses.values())
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        total_train_loss += loss.item()
-
-    # Validate the model
-    model.eval()
-    total_val_loss = 0
-
-    with torch.no_grad():
-        for images, targets in val_loader:
-            images = [i.to(device) for i in images]
-            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
-            losses, detections = model(images, targets)
-            loss = sum(l for l in losses.values())
-
-            total_val_loss += loss.item()
-
-    lr_scheduler.step()
-
-    print(
-        f"Epoch: [{epoch}/{num_epochs}], Train Loss: {total_train_loss:.4f}, Val Loss: {total_val_loss:.4f}"
+    train(
+        model,
+        train_data,
+        val_data,
+        optimizer,
+        lr_scheduler,
+        num_epochs=100,
+        device=device,
     )
 
-    torch.save(model.state_dict(), f"road_damage_detector.pth")
+if args.mode == "test":
+    czech_test_data = RoadDamageDataset(dir="./dataset/czech", split="test")
+    india_test_data = RoadDamageDataset(dir="./dataset/india", split="test")
+    japan_test_data = RoadDamageDataset(dir="./dataset/japan", split="test")
+    norway_test_data = RoadDamageDataset(dir="./dataset/norway", split="test")
+    us_test_data = RoadDamageDataset(dir="./dataset/united_states", split="test")
+    test_data = torch.utils.data.ConcatDataset(
+        [
+            czech_test_data,
+            india_test_data,
+            japan_test_data,
+            norway_test_data,
+            us_test_data,
+        ]
+    )
+
+    predictions, targets = test(model, test_data, device)
+
+    if targets is not None:
+        dataset = us_test_data
+        image_filename = dataset.images[1]
+        image_path = os.path.join(dataset.image_dir, image_filename)
+        annotation = dataset.annotations.get(image_filename, None)
+        visualize_annotation(image_path, annotation)

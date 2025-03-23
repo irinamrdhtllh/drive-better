c5795477b4f581a00e860d2d9d64521aa29a3320
Irina M. S <irinamardhatillah@gmail.com>
2025-03-23 12:26:10 +0700

    Refactor model.py

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from typing import Optional, Dict, List, Tuple
+
 from models.faster_rcnn import fasterrcnn_resnet50_fpn
 
 
@@ -10,5 +12,9 @@ class Model(nn.Module):
 
         self.model = fasterrcnn_resnet50_fpn(num_classes=num_classes)
 
-    def forward(self, images, targets=None):
+    def forward(
+        self,
+        images: List[torch.Tensor],
+        targets: Optional[List[Dict[str, torch.Tensor]]] = None,
+    ) -> Tuple[Dict[str, torch.Tensor], List[Dict[str, torch.Tensor]]]:
         return self.model(images, targets)

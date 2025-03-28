a04498ca2e09afbef23c6b6e09ca1b2f358dc322
Irina M. S <irinamardhatillah@gmail.com>
2025-03-28 11:53:15 +0700

    Update model to use pre-trained weights

--- model.py
+++ model.py
@@ -3,14 +3,24 @@ import torch.nn as nn
 
 from typing import Optional, Dict, List, Tuple
 
-from models.faster_rcnn import fasterrcnn_resnet50_fpn
+from models.faster_rcnn import (
+    FasterRCNN_ResNet50_FPN_Weights,
+    FastRCNNPredictor,
+    fasterrcnn_resnet50_fpn,
+)
 
 
 class Model(nn.Module):
     def __init__(self, num_classes: int):
         super(Model, self).__init__()
 
-        self.model = fasterrcnn_resnet50_fpn(num_classes=num_classes)
+        self.model = fasterrcnn_resnet50_fpn(
+            weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+        )
+
+        # Replace the pre-trained heads to predict 5 classes (4 road damage types + 1 background)
+        in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     def forward(
         self,
